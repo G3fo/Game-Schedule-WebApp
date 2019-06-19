d105e0f9c1b539c69d9040c6a0b80d177edb0869
Mateo Barreiros <matbarreiros@gmail.com>
2019-06-18 22:16:40 -0300

    init

---
+++ games.js
@@ -0,0 +1,142 @@
+var september = [
+    {
+      team1: "U1",
+      team2: "U4",
+      day: 01,
+      month: 09,
+      location: "AJ Katzenmaier",
+      time: "9:30 a.m."
+    },
+    {
+      team1: "U3",
+      team2: "U2",
+      day: 01,
+      month: 09,
+      location: "Greenbay",
+      time: "1:00 p.m."
+    },
+    {
+      team1: "U5",
+      team2: "U6",
+      day: 08,
+      month: 09,
+      location: "Howard A Yeager",
+      time: "9:30 a.m."
+    },
+    {
+      team1: "U6",
+      team2: "U1",
+      day: 08,
+      month: 09,
+      location: "Marjorie P Hart",
+      time: "1:00 p.m."
+    },
+    {
+      team1: "U2",
+      team2: "U4",
+      day: 15,
+      month: 09,
+      location: "North",
+      time: "9:30 a.m."
+    },
+    {
+      team1: "U3",
+      team2: "U5",
+      day: 15,
+      month: 09,
+      location: "AJ Katzenmaier",
+      time: "1:00 p.m."
+    },
+    {
+      team1: "U1",
+      team2: "U3",
+      day: 22,
+      month: 09,
+      location: "South",
+      time: "9:30 a.m."
+    },
+    {
+      team1: "U2",
+      team2: "U6",
+      day: 22,
+      month: 09,
+      location: "Howard A Yeager",
+      time: "1:00 p.m."
+    },
+    {
+      team1: "U4",
+      team2: "U5",
+      day: 22,
+      month: 09,
+      location: "Greenbay",
+      time: "9:30 a.m."
+    }
+  ]
+
+var october = [
+    {
+      team1: "U2",
+      team2: "U5",
+      day: 06,
+      month: 10,
+      location: "Marjorie P Hart",
+      time: "1:00 p.m."
+    },
+    {
+      team1: "U1",
+      team2: "U6",
+      day: 06,
+      month: 10,
+      location: "South",
+      time: "9:30 a.m."
+    },
+    {
+      team1: "U3",
+      team2: "U4",
+      day: 08,
+      month: 10,
+      location: "Howard A Yeager",
+      time: "1:00 p.m."
+    },
+    {
+      team1: "U5",
+      team2: "U1",
+      day: 08,
+      month: 10,
+      location: "Greenbay",
+      time: "9:30 a.m."
+    },
+    {
+      team1: "U6",
+      team2: "U3",
+      day: 20,
+      month: 10,
+      location: "North",
+      time: "1:00 p.m."
+    },
+    {
+      team1: "U2",
+      team2: "U4",
+      day: 20,
+      month: 10,
+      location: "Marjorie P Hart",
+      time: "9:30 a.m."
+    },
+    {
+      team1: "U3",
+      team2: "U1",
+      day: 27,
+      month: 10,
+      location: "AJ Katzenmaier",
+      time: "1:00 p.m."
+    },
+    {
+      team1: "U5",
+      team2: "U6",
+      day: 27,
+      month: 10,
+      location: "Howard A Yeager",
+      time: "9:30 a.m."
+    },
+  ]
+
