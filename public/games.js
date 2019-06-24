9fc63c91159677c283ce5be5a93bedb426d63fae
Mateo Barreiros <matbarreiros@gmail.com>
2019-06-24 16:15:06 -0300

    vue implemented, but not with components

--- public/games.js
+++ public/games.js
@@ -1,142 +1,138 @@
-var september = [
-    {
-      team1: "U1",
-      team2: "U4",
-      day: 01,
-      month: 09,
-      location: "AJ Katzenmaier",
-      time: "9:30 a.m."
-    },
-    {
-      team1: "U3",
-      team2: "U2",
-      day: 01,
-      month: 09,
-      location: "Greenbay",
-      time: "1:00 p.m."
-    },
-    {
-      team1: "U5",
-      team2: "U6",
-      day: 08,
-      month: 09,
-      location: "Howard A Yeager",
-      time: "9:30 a.m."
-    },
-    {
-      team1: "U6",
-      team2: "U1",
-      day: 08,
-      month: 09,
-      location: "Marjorie P Hart",
-      time: "1:00 p.m."
-    },
-    {
-      team1: "U2",
-      team2: "U4",
-      day: 15,
-      month: 09,
-      location: "North",
-      time: "9:30 a.m."
-    },
-    {
-      team1: "U3",
-      team2: "U5",
-      day: 15,
-      month: 09,
-      location: "AJ Katzenmaier",
-      time: "1:00 p.m."
-    },
-    {
-      team1: "U1",
-      team2: "U3",
-      day: 22,
-      month: 09,
-      location: "South",
-      time: "9:30 a.m."
-    },
-    {
-      team1: "U2",
-      team2: "U6",
-      day: 22,
-      month: 09,
-      location: "Howard A Yeager",
-      time: "1:00 p.m."
-    },
-    {
-      team1: "U4",
-      team2: "U5",
-      day: 22,
-      month: 09,
-      location: "Greenbay",
-      time: "9:30 a.m."
-    }
-  ]
-
-var october = [
-    {
-      team1: "U2",
-      team2: "U5",
-      day: 06,
-      month: 10,
-      location: "Marjorie P Hart",
-      time: "1:00 p.m."
-    },
-    {
-      team1: "U1",
-      team2: "U6",
-      day: 06,
-      month: 10,
-      location: "South",
-      time: "9:30 a.m."
-    },
-    {
-      team1: "U3",
-      team2: "U4",
-      day: 08,
-      month: 10,
-      location: "Howard A Yeager",
-      time: "1:00 p.m."
-    },
-    {
-      team1: "U5",
-      team2: "U1",
-      day: 08,
-      month: 10,
-      location: "Greenbay",
-      time: "9:30 a.m."
-    },
-    {
-      team1: "U6",
-      team2: "U3",
-      day: 20,
-      month: 10,
-      location: "North",
-      time: "1:00 p.m."
-    },
-    {
-      team1: "U2",
-      team2: "U4",
-      day: 20,
-      month: 10,
-      location: "Marjorie P Hart",
-      time: "9:30 a.m."
-    },
-    {
-      team1: "U3",
-      team2: "U1",
-      day: 27,
-      month: 10,
-      location: "AJ Katzenmaier",
-      time: "1:00 p.m."
-    },
-    {
-      team1: "U5",
-      team2: "U6",
-      day: 27,
-      month: 10,
-      location: "Howard A Yeager",
-      time: "9:30 a.m."
-    },
-  ]
-
+var matches = [
+  {
+    team1: "U1",
+    team2: "U4",
+    date: "01/09",
+    month: "september",
+    location: "AJ Katzenmaier",
+    time: "9:30 a.m."
+  },
+  {
+    team1: "U3",
+    team2: "U2",
+    date: "01/09",
+    month: "september",
+    location: "Greenbay",
+    time: "1:00 p.m."
+  },
+  {
+    team1: "U5",
+    team2: "U6",
+    date: "08/09",
+    month: "september",
+    location: "Howard A Yeager",
+    time: "9:30 a.m."
+  },
+  {
+    team1: "U6",
+    team2: "U1",
+    date: "08/09",
+    month: "september",
+    location: "Marjorie P Hart",
+    time: "1:00 p.m."
+  },
+  {
+    team1: "U2",
+    team2: "U4",
+    date: "15/09",
+    month: "september",
+    location: "North",
+    time: "9:30 a.m."
+  },
+  {
+    team1: "U3",
+    team2: "U5",
+    date: "15/09",
+    month: "september",
+    location: "AJ Katzenmaier",
+    time: "1:00 p.m."
+  },
+  {
+    team1: "U1",
+    team2: "U3",
+    date: "22/09",
+    month: "september",
+    location: "South",
+    time: "9:30 a.m."
+  },
+  {
+    team1: "U2",
+    team2: "U6",
+    date: "22/09",
+    month: "september",
+    location: "Howard A Yeager",
+    time: "1:00 p.m."
+  },
+  {
+    team1: "U4",
+    team2: "U5",
+    date: "22/09",
+    month: "september",
+    location: "Greenbay",
+    time: "9:30 a.m."
+  },
+  {
+    team1: "U2",
+    team2: "U5",
+    date: "06/10",
+    month: "october",
+    location: "Marjorie P Hart",
+    time: "1:00 p.m."
+  },
+  {
+    team1: "U1",
+    team2: "U6",
+    date: "06/10",
+    month: "october",
+    location: "South",
+    time: "9:30 a.m."
+  },
+  {
+    team1: "U3",
+    team2: "U4",
+    date: "08/10",
+    month: "october",
+    location: "Howard A Yeager",
+    time: "1:00 p.m."
+  },
+  {
+    team1: "U5",
+    team2: "U1",
+    date: "08/10",
+    month: "october",
+    location: "Greenbay",
+    time: "9:30 a.m."
+  },
+  {
+    team1: "U6",
+    team2: "U3",
+    date: "20/10",
+    month: "october",
+    location: "North",
+    time: "1:00 p.m."
+  },
+  {
+    team1: "U2",
+    team2: "U4",
+    date: "20/10",
+    month: "october",
+    location: "Marjorie P Hart",
+    time: "9:30 a.m."
+  },
+  {
+    team1: "U3",
+    team2: "U1",
+    date: "27/10",
+    month: "october",
+    location: "AJ Katzenmaier",
+    time: "1:00 p.m."
+  },
+  {
+    team1: "U5",
+    team2: "U6",
+    date: "27/10",
+    month: "october",
+    location: "Howard A Yeager",
+    time: "9:30 a.m."
+  }
+];
--- public/main.js
+++ public/main.js
@@ -1,1 +1,17 @@
+Vue.component("matches", {
+  props: ["locationURL"],
+  template: ``
+});
 
+Vue.component("months", {
+  template: "<div></div>"
+});
+
+var app = new Vue({
+  el: "#app",
+  data() {
+    return {
+      matches
+    };
+  }
+});
