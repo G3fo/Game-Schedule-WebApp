42fa7913f07d3e518fddcd0ab97bea10f6ba6c77
Mateo Barreiros <matbarreiros@gmail.com>
2019-07-04 22:14:25 -0300

    intentando que funcione

--- public/main.js
+++ public/main.js
@@ -1,8 +1,11 @@
+var mapa = document.getElementById("mapa");
+
 Vue.component("boton", {
-  props: ['team1', 'team2', 'date', 'month', 'location', 'time', 'url', 'id'],
+  props: ["team1", "team2", "date", "month", "location", "time", "url", "id"],
   template: `
               <div class="content">
-                <button class="btn btn-success" type="button" data-toggle="collapse" :data-target="'#match'+month+id"
+                <button onclick="showLandscapeMap(match.url)"
+                  class="btn btn-success" type="button" data-toggle="collapse" :data-target="'#match'+month+id"
                   aria-expanded="false" :aria-controls="'#match'+month+id">
                   <div class="row">
                     <b class="text-left col">
@@ -13,18 +16,17 @@ Vue.component("boton", {
                 </button>
                 </div>
   `
-})
+});
 
-function showLandscapeMap(url, index) {
-  //alert(url);
-  mapa = "<iframe src='" + url + "' allowfullscreen></iframe>";
+function showLandscapeMap(url) {
+  $("#mapa").attr("src", url);
+  mapa.innerHTML = "<iframe src='" + url + "' allowfullscreen></iframe>";
   // $(".btn-estadio-class").removeClass("unaClase");
   // $("#btn-estadio" + index).addClass("unaClase");
 }
 
-
-Vue.component('collapse', {
-  props: ['location', 'time', 'url', 'month', 'id'],
+Vue.component("collapse", {
+  props: ["location", "time", "url", "month", "id"],
   template: `<div class="collapse" :id="'match'+month+id">
               <div class="card card-body">
                 <div class="row">
@@ -36,15 +38,7 @@ Vue.component('collapse', {
               </div>
             </div>
   `
-})
-
-Vue.component('mapa', {
-  template: `<div class="sticky-top" id="mapa">
-        <h3>Select match to show map</h3>
-      </div>`
-})
-
-
+});
 
 var app = new Vue({
   el: "#app",
@@ -52,9 +46,6 @@ var app = new Vue({
     return {
       matches
     };
-
   },
-  methods: {
-
-  },
+  methods: {}
 });
