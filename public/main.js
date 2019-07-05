a6bbb5fda08887a70182265bd09d8763ee2137b8
G3fo <matbarreiros@gmail.com>
2019-07-05 12:08:37 -0300

    intento con componentes

--- public/main.js
+++ public/main.js
@@ -1,8 +1,21 @@
 var mapa = document.getElementById("mapa");
 
+Vue.component("mapa", {
+  props: ["url"],
+  template: `<h3>Select match to show map</h3>
+  <iframe :src="url" frameborder=" 0"></iframe> `
+})
 
 Vue.component("boton", {
   props: ["team1", "team2", "date", "month", "location", "time", "url", "id"],
+  methods: {
+    showLandscapeMap(url) {
+      //$("#mapa").attr("src", url);
+      // mapa.innerHTML = "<iframe src='" + url + "' allowfullscreen></iframe>";
+
+      app.urlSeleccionada = url;
+    }
+  },
   template: `
               <div class="content">
                 <button v-on:click="showLandscapeMap(url)"
@@ -36,19 +49,12 @@ Vue.component("collapse", {
 
 var app = new Vue({
   el: "#app",
-  data() {  
-    return {
-      matches
-    };
+  data: {
+    matches: matches,
+    urlSeleccionada: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.654246110987!2d-87.6312390845582!3d41.90029237922041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34e07f6bac3%3A0x68a82e5d59952c86!2s24+W+Walton+St%2C+Chicago%2C+IL+60610%2C+EE.+UU.!5e0!3m2!1ses-419!2sar!4v1552913537479"
   },
   methods: {
-     showLandscapeMap(url) {
-        $("#mapa").attr("src", url);
-        mapa.innerHTML = "<iframe src='" + url + "' allowfullscreen></iframe>";
-      // $(".btn-estadio-class").removeClass("unaClase");
-      // $("#btn-estadio" + index).addClass("unaClase");
-    }
+
   }
 
 });
-
